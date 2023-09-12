6ee6dbad29d09033dd0d16fc3ff9f9787034ba78
omotomiwa26 <omotomiwa26@gmail.com>
2023-09-12 22:42:42 +0000

    program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_2 import add, sub, mul, div
     from sys import argv, exit
 
-    if len(argv) < 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
