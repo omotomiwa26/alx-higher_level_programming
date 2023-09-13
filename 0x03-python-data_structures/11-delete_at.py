6ac5118ab03496efe112337eb4709d2db8399b16
omotomiwa26 <omotomiwa26@gmail.com>
2023-09-13 03:58:55 +0000

    function that deletes the item at a specific position in a list.

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    
+    new_list = []
+
+    for int in range(len(my_list)):
+        if int != idx:
+            new_list.append(my_list[int])
+
+    return new_list
