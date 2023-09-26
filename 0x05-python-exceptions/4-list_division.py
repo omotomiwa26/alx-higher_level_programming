37c47541790f94a8593ea8ce610fb183011e54e9
omotomiwa26 <omotomiwa26@gmail.com>
2023-09-26 01:19:28 +0000

    function that divides element by element 2 lists.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,6 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             division_result = 0
         except IndexError:
             print("out of range")
+            division_result = 0
         finally:
             new_list.append(division_result)
 
