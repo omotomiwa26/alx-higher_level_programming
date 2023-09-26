acc92b527a0fd2223ff56694feb49726eedca674
omotomiwa26 <omotomiwa26@gmail.com>
2023-09-26 03:10:27 +0000

    function that executes a function safely.

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        stderr.write("Exception: {}\n".format(e))
+        return None
