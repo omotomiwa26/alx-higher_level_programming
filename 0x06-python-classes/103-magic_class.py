907df573948b2f936aab48ecd970891c78784d9f
omotomiwa26 <omotomiwa26@gmail.com>
2023-09-27 04:36:49 +0000

    ByteCode -> Python #5

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a Magic class from a given bytecode"""
+
+from math import pi
+
+
+class MagicClass:
+    """Initializing the attributes for MagicClass"""
+    def __init__(self, radius=0):
+        """Initializing the attributes"""
+        self.__MagicClass__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__MagicClass__radius = radius
+
+    def area(self):
+        """Initializing calculation of area"""
+        return self.__MagicClass__radius ** 2 ** pi
+
+    def circumference(self):
+        """Initializing calculation of circumference"""
+        return 2 * pi * self.__MagicClass__radius
