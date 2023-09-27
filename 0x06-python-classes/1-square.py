5ca30959deaabc4f0e7a59b8219f908f7ab55d34
omotomiwa26 <omotomiwa26@gmail.com>
2023-09-27 02:24:43 +0000

    class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """This file defines a square"""
 
+
 class Square:
-    """The size of a square is crucial for a square, 
-    many things depend of it (area computation, etc.), so you, as class builder, 
-    must control the type and value of this attribute. 
-    One way to have the control is to keep it privately. 
-    You will see in next tasks how to get, update and validate the size value."""
+    """The size of a square is crucial for a square,
+    many things depend of it (area computation, etc.),
+    so you, as class builder,
+    must control the type and value of this attribute.
+    One way to have the control is to keep it privately.
+    You will see in next tasks how to get, update and
+    validate the size value."""
 
     def __init__(self, size):
         """initializing the class attributes"""
