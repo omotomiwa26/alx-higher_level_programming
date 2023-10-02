cf8173e8f075f91f3d999b333a1432f8fc56359c
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-02 16:07:15 +0000

    function that prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    This function accepts two given strings and 
+    prints the result
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ 
+        Define the given strings as 
+        <first_name> and <last_name> 
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
+    
