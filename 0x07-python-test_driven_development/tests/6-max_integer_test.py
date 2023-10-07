f3ceea84f7db6b3ec4447da964464bf7a3ade18f
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-07 00:01:26 +0000

    write unittests for the function def max_integer(list=[]):.

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max(self):
+        # Test if returns the maximum number in the given list
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([5, 6, 7, 8]), 8)
+        self.assertAlmostEqual(max_integer([]), None)
+
+    def test_values(self):
+        # Test if it raises value error when wrong values are used in the list
+        self.assertRaises(TypeError, max_integer, (['1', '2', '3', '4']))
+
+
