92c5b963636a6762f04ae11c362df65b4b944dec
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-11 02:30:20 +0000

    script that adds all arguments to a Python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,18 +11,13 @@ from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-def add_item():
-    """
-    Define and initialise the module 
-    to save and load JSON files
-    """
-    try:
-        existing_list = load_from_json_file('add_item.json')
-    except (FileNotFoundError, j.JSONDecodeError):
-        existing_list = []
+try:
+    existing_list = load_from_json_file('add_item.json')
+except (FileNotFoundError, j.JSONDecodeError):
+    existing_list = []
 
-    new_items = argv[1:]
-    existing_list.extend(new_items)
+new_items = argv[1:]
+existing_list.extend(new_items)
 
-    save_to_json_file(existing_list, 'add_item.json')
+save_to_json_file(existing_list, 'add_item.json')
 
