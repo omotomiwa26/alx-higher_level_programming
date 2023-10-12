b1c39876b9e184e3a5df54d91e26a4943891d0e1
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-12 04:34:57 +0000

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module write a class Square
+that inherits from Rectangle (9-rectangle.py)
+"""
+
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    Define and initialise class square
+
+    Args:
+        Rectangle:
+    """
+    def __init__(self, size):
+        """
+        Args:
+            size:
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        define and initialise area
+        """
+        return super().area()
