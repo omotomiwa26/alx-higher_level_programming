5b942320cab63016aff6f16ec01e7cbb0704808a
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-12 02:47:39 +0000

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module write a class BaseGeometry
+(based on 5-base_geometry.py)
+"""
+
+
+class BaseGeometry:
+    """
+    Define and initialise method
+    """
+
+    def area(self):
+        """
+        Define and initialise the public instance method
+
+        Args:
+            self:Raise execption "area() is not implemeted"
+        """
+
+        raise Exception("area() is not implemented")
