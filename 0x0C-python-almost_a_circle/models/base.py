4bca4a4e3349e4ddbc003cbae47ed4073a12a9df
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-16 03:12:31 +0000

    base file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is the base module 
+for the project
+"""
+
+
+class Base:
+    """
+    Defining the base class with 
+    private attributes
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Define constructor with 
+        instance attribute 
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
