ce7f4264bbdd61969365f5293c4507468d466e80
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-18 00:14:17 +0000

    sqaure file updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,5 +22,14 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
+
     def __str__(self):
         return (F'[Square] {self.id} {self.x}/{self.y} - {self.width}')
