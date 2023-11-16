cd8f154759b44c621d05026f10d8f5b09157b8b1
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-16 00:31:59 +0000

    script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- This script creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.-- If the database hbtn_0d_usa already exists, your script should not fail
+-- If the table states already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+id INT UNIQUE AUTO_INCREMENT NOT NULL,
+PRIMARY KEY(id),
+name VARCHAR(256) NOT NULL
+); 
