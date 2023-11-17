8ba2df55910da13b7eaf6517c3f29af73be7e322
omotomiwa26 <omotomiwa26@gmail.com>
2023-11-17 04:22:39 +0000

    script that lists all the cities of California that can be found in the database hbtn_0d_usa.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,8 +4,6 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 
-USE hbtn_0d_usa;
-
 SELECT c.id, c.name
 FROM cities c
 WHERE c.state_id = (SELECT id FROM states WHERE name = 'California')
