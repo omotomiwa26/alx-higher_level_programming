43f258a034725cb59755d073541fcecf02b76ea1
omotomiwa26 <omotomiwa26@gmail.com>
2023-12-12 21:40:03 +0000

    script that prints a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const argv = process.argv.length - 2;
 
 if (argv === 0) {
   console.log('No argument');
+} else if (argv === 1) {
+  console.log('Argument found');
 } else {
   console.log('Argument found');
 }
