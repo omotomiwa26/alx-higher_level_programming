2fa2459139bc3953ff3153d8215456aab2cfbce9
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-07 23:56:56 -0800

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
 
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
-        content = response.read().decode('utf-8')
+        html = response.read()
 
     print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", (html.decode("utf-8")))
