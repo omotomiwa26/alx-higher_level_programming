a9fea42a57eece81557d1b6c21487e2b6efa1520
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-08 01:05:28 -0800

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     req = r.get(url)
 
-    print("Body response: ")
-    print("\t- type: ", (type(req.text)))
-    print("\t- content: ", (req.text))
+    print("Body response:")
+    print("\t- type:", (type(req.text)))
+    print("\t- content:", (req.text))
