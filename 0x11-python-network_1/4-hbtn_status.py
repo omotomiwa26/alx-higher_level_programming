606e5c4ce808ed6f3ad1c4b2f13bd5b53d78bb88
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-15 02:29:53 -0800

    check

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,23 +2,12 @@
 """
 This Python script fetches https://alx-intranet.hbtn.io/status
 """
-"""
+
 if __name__ == "__main__":
     import requests as r
 
     url = "https://intranet.hbtn.io/status"
     response = r.get(url)
-
     print("Body response:")
     print("\t- type:", type(response.text))
     print("\t- content:", response.text)
-    """
-
-
-import requests
-
-if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
