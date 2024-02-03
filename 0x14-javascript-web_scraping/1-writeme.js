989cefd72ce97093f3056ddc6eb242e40506a25d
omotomiwa26 <omotomiwa26@gmail.com>
2024-02-03 09:56:44 -0800

    script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// This node script writes a string to a file.
+
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node write-file.js <file-path> <string-to-write>');
+  process.exit(1);
+}
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error('Error writting to the file:', err);
+  } else {
+    console.log('File Written Successfully');
+  }
+});
