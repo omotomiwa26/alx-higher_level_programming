32f00c7b298ee0a24fc52f6c842ba395ac22cbeb
omotomiwa26 <omotomiwa26@gmail.com>
2024-02-04 16:01:55 +0000

    movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,25 +4,25 @@
 const request = require('request');
 
 if (process.argv.length !== 3) {
-    console.error('Usage: node get-wedge-antilles-movies.js <api-url>');
-    process.exit(1);
+  console.error('Usage: node get-wedge-antilles-movies.js <api-url>');
+  process.exit(1);
 }
 
 const apiUrl = process.argv[2];
-const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/'
+const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    if (response.statusCode === 200) {
+      const filmsData = JSON.parse(body);
+
+      const moviesWithWedge = filmsData.results.filter(film => film.characters.includes(charUrl));
+
+      console.log(moviesWithWedge.length);
     } else {
-        if (response.statusCode === 200) {
-            const filmsData = JSON.parse(body);
-
-            const moviesWithWedge = filmsData.results.filter(film => film.characters.includes(charUrl));
-
-            console.log(moviesWithWedge.length);
-        } else {
-            console.error('Error:', response.statusCode);
-        }
+      console.error('Error:', response.statusCode);
     }
+  }
 });
