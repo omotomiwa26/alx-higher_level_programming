e360f149a734e52846847747a22cac5f08ab551d
omotomiwa26 <omotomiwa26@gmail.com>
2024-02-04 16:10:23 +0000

    contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// This node script gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node get-webpage-content.js <url> <output-file-path>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (writeError) => {
+      if (writeError) {
+        console.error('Error writing to the file:', writeError);
+      } else {
+        console.log(filePath);
+      }
+    });
+  }
+});
