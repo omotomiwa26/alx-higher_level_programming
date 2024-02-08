f4bc1d96647a84aa66ad9d2fb492e9acd300330b
omotomiwa26 <omotomiwa26@gmail.com>
2024-02-08 01:29:13 +0100

    updates the text color of the <header> element to red without using document.querySelector

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// This script updates the text color of the <header> element to red without using document.querySelector
+
+$(document).ready(function() {
+    $('header').css('color', '#FF0000');
+});
