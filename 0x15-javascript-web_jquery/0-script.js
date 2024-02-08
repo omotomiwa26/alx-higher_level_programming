c973874ac34505d462c479a6e1afa27009f1fcdb
omotomiwa26 <omotomiwa26@gmail.com>
2024-02-08 01:23:17 +0100

    updates the text color of the <header> element to red

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// This Script updates the text color of the <header> element to red 
+
+document.querySelector;
+const headerElement = document.querySelector('header');
+headerElement.style.color = '#FF0000';
